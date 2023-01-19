e325a845e1f85dacbf5ae3d1e57467fdcc1f86c6
Alex Mendes <alexanderhmendes@gmail.com>
2023-01-19 04:08:06 +0000

    feat: export MediaLibraryCroppedImage

--- src/media-library/index.tsx
+++ src/media-library/index.tsx
@@ -1,3 +1,4 @@
 export { MediaLibraryInput } from './MediaLibraryInput';
 export { MediaLibraryOptions } from './options';
 export { MediaLibraryProvider } from './MediaLibraryProvider';
+export { MediaLibraryCroppedImage } from './MediaLibraryCroppedImage';
