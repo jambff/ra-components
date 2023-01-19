080d6000e5583cdc8e8f45925814b956f5166df2
Alex Mendes <alexanderhmendes@gmail.com>
2023-01-19 17:38:05 +0000

    fix: pass croppable option through the media lib provider

--- src/media-library/MediaLibraryProvider.tsx
+++ src/media-library/MediaLibraryProvider.tsx
@@ -19,6 +19,7 @@ export const MediaLibraryProvider: FC<MediaLibraryProviderProps> = ({
   maxSize,
   aspectRatio,
   sort,
+  croppable,
 }) => {
   const value = useMemo(
     (): MediaLibraryOptions => ({
@@ -30,6 +31,7 @@ export const MediaLibraryProvider: FC<MediaLibraryProviderProps> = ({
       maxSize,
       aspectRatio,
       sort,
+      croppable,
     }),
     [
       supabase,
@@ -40,6 +42,7 @@ export const MediaLibraryProvider: FC<MediaLibraryProviderProps> = ({
       maxSize,
       aspectRatio,
       sort,
+      croppable,
     ],
   );
 
