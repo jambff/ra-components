12d6837b9f9f5147f78e35b87a158f75493263b5
Alex Mendes <alexanderhmendes@gmail.com>
2023-01-29 23:18:19 +0000

    fix: inline step form iterator fields

--- src/inputs/StepFormIterator.tsx
+++ src/inputs/StepFormIterator.tsx
@@ -19,7 +19,7 @@ export const StepFormIterator: FC<StepFormIteratorProps> = ({
       sx={{
         flex: 1,
         '.RaSimpleFormIterator-form': { flex: 1 },
-        '& .MuiFormControl-root': { width: '100%' },
+        '& .ra-input': { width: restProps.inline ? 'auto' : '100%' },
         border: `1px solid ${theme.palette.grey[300]}`,
         padding: `${theme.spacing(1.5)} ${theme.spacing(2)}`,
         marginTop: theme.spacing(1),
