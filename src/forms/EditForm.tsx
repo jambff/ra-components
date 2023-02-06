914ca19db85919b6f96f5c07ed6046c326610974
Alex Mendes <alexanderhmendes@gmail.com>
2023-02-06 23:11:58 +0000

    feat: add title to all edit forms

--- src/forms/EditForm.tsx
+++ src/forms/EditForm.tsx
@@ -8,17 +8,35 @@ import {
   SimpleForm,
   SimpleFormProps,
   useNotify,
+  useRecordContext,
   useRedirect,
+  useResourceContext,
 } from 'react-admin';
 
 type EditFormProps = Omit<EditProps, 'resource'> & {
   children: ReactNode;
   form?: Omit<SimpleFormProps, 'children'>;
+  titleSource?: string;
+};
+
+const getResourceLabel = (resource: string) => capitalCase(singular(resource));
+
+const Title = ({ source }: { source?: string }) => {
+  const record = useRecordContext();
+  const resource = useResourceContext();
+
+  return (
+    <span>
+      Edit {getResourceLabel(resource)} -{' '}
+      {record && source ? `${record[source]}` : record.id}
+    </span>
+  );
 };
 
 export const EditForm: FC<EditFormProps> = ({
   children,
   form,
+  titleSource,
   ...restProps
 }: EditFormProps) => {
   const notify = useNotify();
@@ -29,12 +47,13 @@ export const EditForm: FC<EditFormProps> = ({
       throw new Error('No resource found');
     }
 
-    notify(`${capitalCase(singular(resource))} updated`);
+    notify(`${getResourceLabel(resource)} updated`);
     redirect('list', resource, data.id, data);
   };
 
   return (
     <Edit
+      title={<Title source={titleSource} />}
       mutationOptions={{ onSuccess }}
       mutationMode="pessimistic"
       {...restProps}>
